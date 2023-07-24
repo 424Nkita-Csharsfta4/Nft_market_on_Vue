f2990969a5e99e8c3675cf8562678f37eae328bc
Nikita_Developer_PMR <103760832+424Nkita-Csharsfta4@users.noreply.github.com>
2023-07-24 22:05:49 +0300

    Update Chat.vue

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -25,83 +25,83 @@
 </template>
   
 <script setup>
-import { ref, onMounted } from "vue";
-// import firebase from "firebase/app";
-// import "firebase/auth";
-import io from "socket.io-client";
+// import { ref, onMounted } from "vue";
+// // import firebase from "firebase/app";
+// // import "firebase/auth";
+// import io from "socket.io-client";
 
-const user = ref(null);
-const messages = ref([]);
-const newMessage = ref("");
+// const user = ref(null);
+// const messages = ref([]);
+// const newMessage = ref("");
 
-//Import the functions you need from the SDKs you need
-// import firebase from 'firebase/app';
-// import 'firebase/auth';
+// //Import the functions you need from the SDKs you need
+// // import firebase from 'firebase/app';
+// // import 'firebase/auth';
 
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyB_TkrcU0KQIemHrcX5rZlcPEPIZWITRcY",
-//   authDomain: "vuelogin-2fe32.firebaseapp.com",
-//   projectId: "vuelogin-2fe32",
-//   storageBucket: "vuelogin-2fe32.appspot.com",
-//   messagingSenderId: "355959677018",
-//   appId: "1:355959677018:web:6270fd2c6e38fe2709f863",
-//   measurementId: "G-QZMEEY9P90"
+// // // Your web app's Firebase configuration
+// // const firebaseConfig = {
+// //   apiKey: "AIzaSyB_TkrcU0KQIemHrcX5rZlcPEPIZWITRcY",
+// //   authDomain: "vuelogin-2fe32.firebaseapp.com",
+// //   projectId: "vuelogin-2fe32",
+// //   storageBucket: "vuelogin-2fe32.appspot.com",
+// //   messagingSenderId: "355959677018",
+// //   appId: "1:355959677018:web:6270fd2c6e38fe2709f863",
+// //   measurementId: "G-QZMEEY9P90"
+// // };
+
+// // // Initialize Firebase
+// // firebase.initializeApp(firebaseConfig);
+
+// // firebase.initializeApp(firebaseConfig);
+
+// // Firebase Authentication
+// const auth = firebase.auth();
+
+// auth.onAuthStateChanged((authUser) => {
+//     if (authUser) {
+//         user.value = authUser;
+//     } else {
+//         user.value = null;
+//     }
+// });
+
+// // WebSocket Communication
+// const socket = io("ws://localhost:3000"); // Replace with your WebSocket server URL
+
+// const sendMessage = () => {
+//     if (newMessage.value.trim() !== "") {
+//         socket.emit("message", {
+//             sender: user.value.displayName,
+//             text: newMessage.value.trim(),
+//         });
+//         newMessage.value = "";
+//     }
 // };
 
-// // Initialize Firebase
-// firebase.initializeApp(firebaseConfig);
+// socket.on("message", (message) => {
+//     messages.value.push(message);
+// });
 
-// firebase.initializeApp(firebaseConfig);
+// // Sign In with Google
+// const signInWithGoogle = async () => {
+//     const provider = new firebase.auth.GoogleAuthProvider();
+//     try {
+//         await auth.signInWithPopup(provider);
+//     } catch (error) {
+//         console.error("Google Sign-In Error:", error);
+//     }
+// };
 
-// Firebase Authentication
-const auth = firebase.auth();
+// onMounted(() => {
+//     // Set up WebSocket connection
+//     socket.on("connect", () => {
+//         console.log("WebSocket connected");
+//     });
 
-auth.onAuthStateChanged((authUser) => {
-    if (authUser) {
-        user.value = authUser;
-    } else {
-        user.value = null;
-    }
-});
-
-// WebSocket Communication
-const socket = io("ws://localhost:3000"); // Replace with your WebSocket server URL
-
-const sendMessage = () => {
-    if (newMessage.value.trim() !== "") {
-        socket.emit("message", {
-            sender: user.value.displayName,
-            text: newMessage.value.trim(),
-        });
-        newMessage.value = "";
-    }
-};
-
-socket.on("message", (message) => {
-    messages.value.push(message);
-});
-
-// Sign In with Google
-const signInWithGoogle = async () => {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    try {
-        await auth.signInWithPopup(provider);
-    } catch (error) {
-        console.error("Google Sign-In Error:", error);
-    }
-};
-
-onMounted(() => {
-    // Set up WebSocket connection
-    socket.on("connect", () => {
-        console.log("WebSocket connected");
-    });
-
-    socket.on("disconnect", () => {
-        console.log("WebSocket disconnected");
-    });
-});
+//     socket.on("disconnect", () => {
+//         console.log("WebSocket disconnected");
+//     });
+// });
 </script>
   
 <style>
