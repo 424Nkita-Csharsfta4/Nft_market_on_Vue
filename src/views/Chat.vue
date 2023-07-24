f3e89632f0ff68165d6f15f95dc93d1ab6bee280
Nikita_Developer_PMR <103760832+424Nkita-Csharsfta4@users.noreply.github.com>
2023-07-24 17:51:20 +0300

    Update Chat.vue

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -35,24 +35,24 @@ const messages = ref([]);
 const newMessage = ref("");
 
 //Import the functions you need from the SDKs you need
-import firebase from 'firebase/app';
-import 'firebase/auth';
+// import firebase from 'firebase/app';
+// import 'firebase/auth';
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyB_TkrcU0KQIemHrcX5rZlcPEPIZWITRcY",
-  authDomain: "vuelogin-2fe32.firebaseapp.com",
-  projectId: "vuelogin-2fe32",
-  storageBucket: "vuelogin-2fe32.appspot.com",
-  messagingSenderId: "355959677018",
-  appId: "1:355959677018:web:6270fd2c6e38fe2709f863",
-  measurementId: "G-QZMEEY9P90"
-};
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyB_TkrcU0KQIemHrcX5rZlcPEPIZWITRcY",
+//   authDomain: "vuelogin-2fe32.firebaseapp.com",
+//   projectId: "vuelogin-2fe32",
+//   storageBucket: "vuelogin-2fe32.appspot.com",
+//   messagingSenderId: "355959677018",
+//   appId: "1:355959677018:web:6270fd2c6e38fe2709f863",
+//   measurementId: "G-QZMEEY9P90"
+// };
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+// // Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
 
-firebase.initializeApp(firebaseConfig);
+// firebase.initializeApp(firebaseConfig);
 
 // Firebase Authentication
 const auth = firebase.auth();
